153fc63f3628840894b6381f16a159ec77b49957
MATTEO ISOLDI s318980 <s318980@studenti.polito.it>
2023-05-15 10:19:28 +0200

    Added register unit test

--- code/test/auth.unit.test.js
+++ code/test/auth.unit.test.js
@@ -2,14 +2,63 @@ import request from 'supertest';
 import { app } from '../app';
 import { User } from '../models/User.js';
 import jwt from 'jsonwebtoken';
+import { response } from 'express';
 const bcrypt = require("bcryptjs")
 
 jest.mock("bcryptjs")
 jest.mock('../models/User.js');
 
 describe('register', () => { 
-    test('Dummy test, change it', () => {
-        expect(true).toBe(true);
+    test('Regular user Pippo registration: Test #1', async () => {
+        await request(app).post("/api/register").send({
+            username : "Pippo",
+            email : "s239834@studenti.polito.it",
+            password : "12345" 
+        }).then(response => {
+            expect(response.statusCode).toBe(200);
+        }); 
+    });
+
+    test('Regular user Pluto registration: Test #2', async () => {
+        await request(app).post("/api/register").send({
+            username : "Pluto",
+            email : "s392892@studenti.polito.it",
+            password : "12345" 
+        }).then(response => {
+            expect(response.statusCode).toBe(200);
+        }); 
+    });
+
+    test('Regular user Paperino registration: Test #3', async () => {
+        await request(app).post("/api/register").send({
+            username : "Paperino",
+            email : "s498239@studenti.polito.it",
+            password : "12345" 
+        }).then(response => {
+            expect(response.statusCode).toBe(200);
+        }); 
+    });
+
+    test('Already existing username Pluto: Error test #1', async () => {
+        await request(app).post("/api/register").send({
+            username : "Pluto",
+            email : "s573367@studenti.polito.it",
+            password : "12345" 
+        }).then(response => {
+            console.log('Response Body :', JSON.stringify(response.body, null, 2));
+            expect(response.statusCode).toBe(400);
+        }); 
+    });
+
+    test('Already existing email: Error test #1', async () => {
+        await request(app).post("/api/register").send({
+            username : "Topolino",
+            email : "s498239@studenti.polito.it",
+            password : "12345" 
+        }).then(response => {
+            console.log('Response Body :', JSON.stringify(response.body, null, 2));
+            expect(response.statusCode).toBe(400);
+        }); 
     });
 });
 
