bd1e2ecd85cee40f8da916d23f6a59d0add2c249
Matteo Isoldi s318980 <s318980@studenti.polito.it>
2023-05-29 10:19:15 +0200

    Fixed functions for new requirements

--- code/controllers/auth.js
+++ code/controllers/auth.js
@@ -5,14 +5,30 @@ import { verifyAuth } from './utils.js';
 
 /**
  * Register a new user in the system
+  - Request Parameters: None
   - Request Body Content: An object having attributes `username`, `email` and `password`
+    - Example: `{username: "Mario", email: "mario.red@email.com", password: "securePass"}`
   - Response `data` Content: A message confirming successful insertion
+    - Example: `res.status(200).json({data: {message: "User added successfully"}})`
   - Optional behavior:
-    - error 400 is returned if there is already a user with the same username and/or email
+    - Returns a 400 error if the request body does not contain all the necessary attributes
+    - Returns a 400 error if at least one of the parameters in the request body is an empty string
+    - Returns a 400 error if the email in the request body is not in a valid email format
+    - Returns a 400 error if the username in the request body identifies an already existing user
+    - Returns a 400 error if the email in the request body identifies an already existing user
  */
 export const register = async (req, res) => {
     try {
+        
+        // Check for presence of all necessary attributes
+        if (!req.body.hasOwnProperty("username") || !req.body.hasOwnProperty("email") || !req.body.hasOwnProperty("password"))
+            return res.status(400).json({ error: "Missing parameters in the body of the request" });
+
         const { username, email, password } = req.body;
+        
+        // Check for presence of values in all necessary attributes
+        if (username === "" || email === "" || password === "")
+            return res.status(400).json({ error: "Missing parameters in the body of the request" });
         
         // Check if a user with same mail or username as already been registered before
         const existingMail = await User.findOne({ email: req.body.email });
@@ -36,7 +52,6 @@ export const register = async (req, res) => {
 
         let returnValue = {
             data: { message : "User added successfully"},
-            message: res.locals.message
         };
 
         res.status(200).json(returnValue);
@@ -47,14 +62,30 @@ export const register = async (req, res) => {
 
 /**
  * Register a new user in the system with an Admin role
-  - Request Body Content: An object having attributes `username`, `email` and `password`
+  - Request Parameters: None
+  - Request Body Content: An object having attributes `username`, `email` and `password`  
+    - Example: `{username: "admin", email: "admin@email.com", password: "securePass"}`
   - Response `data` Content: A message confirming successful insertion
+    - Example: `res.status(200).json({data: {message: "User added successfully"}})`
   - Optional behavior:
-    - error 400 is returned if there is already a user with the same username and/or email
+    - Returns a 400 error if the request body does not contain all the necessary attributes
+    - Returns a 400 error if at least one of the parameters in the request body is an empty string
+    - Returns a 400 error if the email in the request body is not in a valid email format
+    - Returns a 400 error if the username in the request body identifies an already existing user
+    - Returns a 400 error if the email in the request body identifies an already existing user
  */
 export const registerAdmin = async (req, res) => {
     try {
+
+        // Check for presence of all necessary attributes
+        if (!req.body.hasOwnProperty("username") || !req.body.hasOwnProperty("email") || !req.body.hasOwnProperty("password"))
+            return res.status(400).json({ error: "Missing parameters in the body of the request" });
+
         const { username, email, password } = req.body;
+
+        // Check for presence of values in all necessary attributes
+        if (username === "" || email === "" || password === "")
+            return res.status(400).json({ error: "Missing parameters in the body of the request" });
 
         // Check if a user with same mail or username as already been registered before
         const existingMail = await User.findOne({ email: req.body.email });
@@ -79,7 +110,6 @@ export const registerAdmin = async (req, res) => {
         
         let returnValue = {
             data: { message : "Admin added successfully" },
-            message: res.locals.message
         };
 
         res.status(200).json(returnValue);
@@ -91,16 +121,35 @@ export const registerAdmin = async (req, res) => {
 
 /**
  * Perform login 
+  - Request Parameters: None
   - Request Body Content: An object having attributes `email` and `password`
+    - Example: `{email: "mario.red@email.com", password: "securePass"}`
   - Response `data` Content: An object with the created accessToken and refreshToken
+    - Example: `res.status(200).json({data: {accessToken: accessToken, refreshToken: refreshToken}})`
   - Optional behavior:
-    - error 400 is returned if the user does not exist
-    - error 400 is returned if the supplied password does not match with the one in the database
+    - Returns a 400 error if the request body does not contain all the necessary attributes
+    - Returns a 400 error if at least one of the parameters in the request body is an empty string
+    - Returns a 400 error if the email in the request body is not in a valid email format
+    - Returns a 400 error if the email in the request body does not identify a user in the database
+    - Returns a 400 error if the supplied password does not match with the one in the database
  */
-export const login = async (req, res) => {
+export const login = async (req, res) => { 
+    
+    // Check for presence of all necessary attributes
+    if (!req.body.hasOwnProperty("email") || !req.body.hasOwnProperty("password"))
+        return res.status(400).json({ error: "Missing parameters in the body of the request" });
     
     const { email, password } = req.body;
     const cookie = req.cookies;
+
+    // Check for presence of values in all necessary attributes
+    if (email === "" || password === "")
+        return res.status(400).json({ error: "Missing parameters in the body of the request" });
+    
+    // Check if the email is valid
+    const validMail = verifyEmail(req);
+    if (!validMail)
+        return res.status(400).json({ error: "The email is not valid, please check it"});
 
     const existingUser = await User.findOne({ email: email });
     if (!existingUser) 
@@ -134,7 +183,7 @@ export const login = async (req, res) => {
         const savedUser = await existingUser.save()
         res.cookie("accessToken", accessToken, { httpOnly: true, domain: "localhost", path: "/api", maxAge: 60 * 60 * 1000, sameSite: "none", secure: true })
         res.cookie('refreshToken', refreshToken, { httpOnly: true, domain: "localhost", path: '/api', maxAge: 7 * 24 * 60 * 60 * 1000, sameSite: 'none', secure: true })
-        res.status(200).json({ data: { accessToken: accessToken, refreshToken: refreshToken }, message: res.locals.message })
+        res.status(200).json({ data: { accessToken: accessToken, refreshToken: refreshToken }})
     
     } catch (error) {
         res.status(400).json({ error: error })
@@ -144,10 +193,13 @@ export const login = async (req, res) => {
 /**
  * Perform logout
   - Auth type: Simple
+  - Request Parameters: None
   - Request Body Content: None
   - Response `data` Content: A message confirming successful logout
+    - Example: `res.status(200).json({data: {message: "User logged out"}})`
   - Optional behavior:
-    - error 400 is returned if the user does not exist
+    - Returns a 400 error if the request does not have a refresh token in the cookies
+    - Returns a 400 error if the refresh token in the request's cookies does not represent a user in the database
  */
 export const logout = async (req, res) => {
 
@@ -164,8 +216,7 @@ export const logout = async (req, res) => {
         // Once user has been confirmed to exist, check if the token is expired
         const decodedRefreshToken = jwt.verify(refreshToken, process.env.ACCESS_KEY);
         let returnValue = {
-            data: { message : "Logged out"},
-            message: res.locals.message
+            data: { message : "User logged out"},
         };
         
         user.refreshToken = null
