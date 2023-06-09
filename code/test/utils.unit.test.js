71fd8a94df9489bd7d1360e41fb0de5219f0002a
Matteo Isoldi s318980 <s318980@studenti.polito.it>
2023-06-09 15:32:36 +0200

    New tests

--- code/test/utils.unit.test.js
+++ code/test/utils.unit.test.js
@@ -193,6 +193,137 @@ describe("verifyAuth", () => {
         expect(result).toHaveProperty("cause");
     });
 
+    test('Simple authentication: missing parameter in the access token error', () => {
+        const accessToken = "testAccessToken";
+        const refreshToken = "testRefreshToken";
+
+        const decodedAccessToken = {
+            id: "123456",
+            email: "s256789@studenti.polito.it",
+            role: "Regular"
+        }
+
+        const decodedRefreshToken = {
+            id: "123456",
+            username: "Topolino",
+            email: "s256789@studenti.polito.it",
+            role: "Regular"
+        }
+
+        //Since we are calling the function directly, we need to manually define a request object with all the necessary parameters (route params, body, cookies, url)
+        const req = {
+            body: {},
+            cookies: { accessToken: accessToken, refreshToken: refreshToken }
+        }
+
+        //The same reasoning applies for the response object: we must manually define the functions used and then check if they are called (and with which values)
+        const res = {} 
+
+        // Info object for the verifyAuth function
+        const info = {
+            authType: "Simple"
+        }
+
+        jwt.verify = jest.fn().mockImplementation(decodedAccessToken)
+                              .mockImplementation(decodedRefreshToken);
+
+        // Call the function
+        const result = verifyAuth(req, res, info);
+        
+        // Verify the results
+        expect(result).toHaveProperty("authorized", false);
+        expect(result).toHaveProperty("cause");
+    });
+
+    test('Simple authentication: missing parameter in the refresh token error', () => {
+        const accessToken = "testAccessToken";
+        const refreshToken = "testRefreshToken";
+
+        const decodedAccessToken = {
+            id: "123456",
+            username: "Topolino",
+            email: "s256789@studenti.polito.it",
+            role: "Regular"
+        }
+
+        const decodedRefreshToken = {
+            id: "123456",
+            username: "Topolino",
+            role: "Regular"
+        }
+
+        //Since we are calling the function directly, we need to manually define a request object with all the necessary parameters (route params, body, cookies, url)
+        const req = {
+            body: {},
+            cookies: { accessToken: accessToken, refreshToken: refreshToken }
+        }
+
+        //The same reasoning applies for the response object: we must manually define the functions used and then check if they are called (and with which values)
+        const res = {
+            cookie: jest.fn()
+        } 
+
+        // Info object for the verifyAuth function
+        const info = {
+            authType: "Simple"
+        }
+
+        jwt.verify = jest.fn().mockImplementation(decodedAccessToken)
+                              .mockImplementation(decodedRefreshToken);
+
+        // Call the function
+        const result = verifyAuth(req, res, info);
+        
+        // Verify the results
+        expect(result).toHaveProperty("authorized", false);
+        expect(result).toHaveProperty("cause");
+    });
+
+    test('Simple authentication: access token expired', () => {
+        const accessToken = "testAccessToken";
+        const refreshToken = "testRefreshToken";
+
+        const decodedAccessToken = {
+            id: "123456",
+            username: "Topolino",
+            email: "s256789@studenti.polito.it",
+            role: "Regular"
+        }
+
+        const decodedRefreshToken = {
+            id: "123456",
+            username: "Topolino",
+            email: "s256789@studenti.polito.it",
+            role: "Regular"
+        }
+
+        //Since we are calling the function directly, we need to manually define a request object with all the necessary parameters (route params, body, cookies, url)
+        const req = {
+            body: {},
+            cookies: { accessToken: accessToken, refreshToken: refreshToken }
+        }
+
+        //The same reasoning applies for the response object: we must manually define the functions used and then check if they are called (and with which values)
+        const res = {
+            cookies: jest.fn()
+        } 
+
+        // Info object for the verifyAuth function
+        const info = {
+            authType: "Simple"
+        }
+
+        jwt.verify = jest.fn().mockImplementation(() => {throw new Error("TokenExpiredError");})
+                              .mockImplementation(decodedRefreshToken);
+
+        // Call the function
+        const result = verifyAuth(req, res, info);
+        
+        // Verify the results
+        expect(result).toHaveProperty("authorized", true);
+        expect(result).toHaveProperty("cause");
+    });
+
     test('Simple authentication: access token and refresh token expired', () => {
         const accessToken = "testAccessToken";
         const refreshToken = "testRefreshToken";
